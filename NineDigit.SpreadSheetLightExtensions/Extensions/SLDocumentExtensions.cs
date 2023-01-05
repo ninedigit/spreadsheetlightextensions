5113ecab4a59905fdcb2b6075c74f6b17b7be383
yurislav <juraj.malach@gmail.com>
2023-01-05 16:33:06 +0100

    new extensions added

--- NineDigit.SpreadSheetLightExtensions/Extensions/SLDocumentExtensions.cs
+++ NineDigit.SpreadSheetLightExtensions/Extensions/SLDocumentExtensions.cs
@@ -1,4 +1,5 @@
-﻿using SpreadsheetLight;
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using SpreadsheetLight;
 using System;
 using System.Collections.Generic;
 
@@ -51,6 +52,58 @@ namespace NineDigit.SpreadsheetLightExtensions
             var style = self.GetCellStyle(cell);
             style.FormatCode = format;
             self.SetCellStyle(cell, style);
+        }
+
+        public static bool SetCellBackground(this SLDocument doc, CellReference cellReference, System.Drawing.Color color)
+        {
+            if (doc is null)
+                throw new ArgumentNullException(nameof(doc));
+
+            if (cellReference is null)
+                throw new ArgumentNullException(nameof(cellReference));
+
+            var style = doc.GetCellStyle(cellReference);
+            style.Fill.SetPattern(PatternValues.Solid, ForegroundColor: color, BackgroundColor: color);
+            return doc.SetCellStyle(cellReference, style);
+        }
+
+        public static bool SetCellFontSize(this SLDocument doc, CellReference cellReference, int fontSize)
+        {
+            if (doc is null)
+                throw new ArgumentNullException(nameof(doc));
+
+            if (cellReference is null)
+                throw new ArgumentNullException(nameof(cellReference));
+
+            var style = doc.GetCellStyle(cellReference);
+            style.Font.FontSize = fontSize;
+            return doc.SetCellStyle(cellReference, style);
+        }
+
+        public static bool SetCellFontColor(this SLDocument doc, CellReference cellReference, System.Drawing.Color fontColor)
+        {
+            if (doc is null)
+                throw new ArgumentNullException(nameof(doc));
+
+            if (cellReference is null)
+                throw new ArgumentNullException(nameof(cellReference));
+
+            var style = doc.GetCellStyle(cellReference);
+            style.Font.FontColor = fontColor;
+            return doc.SetCellStyle(cellReference, style);
+        }
+
+        public static bool SetCellBold(this SLDocument doc, CellReference cellReference, bool bold = true)
+        {
+            if (doc is null)
+                throw new ArgumentNullException(nameof(doc));
+
+            if (cellReference is null)
+                throw new ArgumentNullException(nameof(cellReference));
+
+            var style = doc.GetCellStyle(cellReference);
+            style.Font.Bold = bold;
+            return doc.SetCellStyle(cellReference, style);
         }
     }
 }
